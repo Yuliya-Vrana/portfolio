d46ed9c714dac80ff3183070649432a1c32145ca
Julia Tselicova <malipuha@list.ru>
2022-08-02 01:21:58 +1000

    module carousel ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import ibg from './modules/ibg'
 import light from './modules/light.js'
 import menu from './modules/menu.js'
+import carousel from './modules/carousel.js'
 
 ibg()
 light()
 menu()
+carousel()
