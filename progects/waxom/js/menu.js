18fc4df5e49d651d778ded5c55cfdcc41374ad57
Julia Tselicova <malipuha@list.ru>
2022-08-20 20:52:36 +1000

    очистка ветки

--- progects/waxom/js/menu.js
+++
@@ -1,9 +0,0 @@
-let menuToggle = document.querySelector('.toggle');
-let navigation = document.querySelector('.navigation');
-let menuTitle = document.querySelector('.menu__burger--link');
-
-menuToggle.onclick = function(){
-	menuToggle.classList.toggle('active');
-	navigation.classList.toggle('active');
-	menuTitle.classList.toggle('active');
-}
