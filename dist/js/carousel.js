2a71874e0fcf677dbcd1a3013cb2b011998f1c88
Julia Tselicova <malipuha@list.ru>
2022-08-20 14:00:11 +1000

    раздельные js, и отключила блок где можно написать, так как гит не держит php

---
+++ dist/js/carousel.js
@@ -0,0 +1,57 @@
+    let sliderToShow = 3
+    const container = document.querySelector('.slider')
+    const track = container.querySelector('.carousel')
+    const elements = track.querySelectorAll('.slider_item')
+
+    const arrowLeft = document.querySelector('.arrow-left')
+    const arrowRight = document.querySelector('.arrow-right')
+
+    const clientWidthScreen = screen.width
+
+    if(clientWidthScreen >= 1250){
+        sliderToShow = 3
+    } else if(clientWidthScreen < 1250 && clientWidthScreen >= 950){
+        sliderToShow = 2
+    } else if(clientWidthScreen < 950 ){
+        sliderToShow = 1
+    }
+
+    const elemWidth = Math.floor(container.clientWidth / sliderToShow)
+
+    let position = -elemWidth
+     
+    elements.forEach(elem => { 
+        elem.style.minWidth = `calc(${elemWidth}px - 0px)`
+    })
+
+    const setPosition = () => {
+   
+        let scroll = - (elements.length - sliderToShow) * elemWidth
+
+        if(position > 0){
+            position = scroll
+        }
+
+        if(position < scroll){
+            position = 0
+        }
+
+        track.style.transform = `translateX(${position}px)`      
+    }
+
+    
+    arrowLeft.addEventListener('click', () => {
+        position += Math.floor(elemWidth)
+        setPosition()
+
+    })
+
+    arrowRight.addEventListener('click', () => {
+        position -= Math.floor(elemWidth)
+        setPosition()
+
+    })
+
+
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,8 @@
 import ibg from './modules/ibg'
 import light from './modules/light.js'
 import menu from './modules/menu.js'
-import carousel from './modules/carousel.js'
-import contactModal from './modules/contactModal.js' 
-
-const page = document.querySelector('h1')
 
 ibg()
 light()
 menu()
 
-
-//carousel()
-contactModal()
